2ab727dfc0bb179f53dbc74adf5a4108dee4062e
Jeck0507 <wjdgur050700@naver.com>
2023-07-13 11:27:23 +0900

    1차 개발 완료

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
-const express = require('express'); 
+const express = require("express");
 const app = express();
-const bodyParser= require('body-parser')
-const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require("body-parser");
+// const MongoClient = require("mongodb").MongoClient;
 
-app.use(bodyParser.urlencoded({extended: true})) 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect('mongodb+srv://admin:im5967im@cluster0.xtntkvz.mongodb.net/?retryWrites=true&w=majority', function(err, client){
-    app.listen('8080', function(){
-        console.log('listening on 8080');
-    });
-    
+app.listen("8080", function () {
+    console.log("listening on 8080");
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', function(req, res) { 
-    res.sendFile(__dirname +'/views/start.html')
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/start.html");
+});
 
-  })
+app.get("/inputs", function (req, res) {
+    res.sendFile(__dirname + "/views/inputs.html");
+});
 
-app.get('/inputs', function(req, res) { 
-    res.sendFile(__dirname +'/views/inputs.html')
-  })
-
-app.post('/man-add',function(req, res){
+app.post("/man-add", function (req, res) {
     console.log(req.body.man);
-    var gender = 'man'
-})
+    var gender = "man";
+});
 
-app.post('/woman-add',function(req, res){
+app.post("/woman-add", function (req, res) {
     console.log(req.body.woman);
-    var gender = 'woman'
-})
+    var gender = "woman";
+});
 
-
-app.get('/info', function(req, res) { 
-    res.sendFile(__dirname +'/views/Developer Info 82acc3b5a41a452a8e45a33f062131b6.html')
-  })
+app.get("/info", function (req, res) {
+    res.sendFile(
+        __dirname +
+            "/views/Developer Info 82acc3b5a41a452a8e45a33f062131b6.html"
+    );
+});
